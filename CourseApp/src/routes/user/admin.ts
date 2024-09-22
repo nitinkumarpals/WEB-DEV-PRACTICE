1d94a97c9fa5cfd542f0972aae804b4d0d8ab6cc
nitinkumarpals <nitinpal900@gmail.com>
2024-09-22 13:31:12 +0530

    feat(admin): Added Zod validation for admin signup and created signup schema

--- CourseApp/src/routes/user/admin.ts
+++ CourseApp/src/routes/user/admin.ts
@@ -1,14 +1,60 @@
 import { Router } from "express";
 export const adminRouter = Router();
-// const { adminModel } = require("../db");
-
-adminRouter.post("/signup", function (req, res) {
-  res.json({
-    message: "signup endpoint",
-  });
+import { adminModel, courseModel } from "../../db/db";
+import { signupSchema } from "../../schema/signupSchema";
+adminRouter.post("/signup", async function (req, res) {
+  try {
+    const body = req.body;
+    const parsedBody = signupSchema.safeParse(body);
+    if (!parsedBody.success) {
+      const errorMessages = parsedBody.error.errors
+        .map((err) => err.message)
+        .join(", ");
+      return res.status(400).json({
+        success: false,
+        message: errorMessages,
+      });
+    }
+    const { email, password, firstName, lastName } = parsedBody.data;
+    const admin = await adminModel.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+    // Create a new course document with the creatorId set to the adminId
+    const course = await courseModel.create({
+      title: "Default Course Title",
+      description: "Default Course Description",
+      price: 0,
+      imageUrl: "",
+      creatorId: admin._id,
+    });
+    res.status(200).json({
+      admin,
+      course,
+      message: "Admin and course created successfully",
+    });
+  } catch (error : Error | any) {
+    if (error instanceof Error && (error as any).code === 11000) {
+      // Unique constraint error code
+      return res.status(400).json({
+        success: false,
+        error: "Email already exists",
+        message: "Email already in use",
+      });
+    } else {
+      // Handle other errors
+      return res.status(500).json({
+        success: false,
+        error: error.message,
+        message: "Internal server error",
+      });
+    }
+  }
 });
 
-adminRouter.post("/signin", function (req, res) {
+adminRouter.get("/signin", function (req, res) {
   res.json({
     message: "signup endpoint",
   });
@@ -31,5 +77,3 @@ adminRouter.get("/course/bulk", function (req, res) {
     message: "signup endpoint",
   });
 });
-
-
---
+++ CourseApp/src/schema/signupSchema.ts
@@ -0,0 +1,7 @@
+import { z } from "zod";
+export const signupSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
