cdc808baf3001b94f387bb0188361cf7985937ba
nitinkumarpals <nitinpal900@gmail.com>
2024-12-28 15:05:44 +0530

    commit

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { RootState } from "./redux/store";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import { decrement, increment } from "./features/counter/counterSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const count = useAppSelector((state: RootState) => state.counter.value);
+  const dispatch = useAppDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={handleIncrement}>+</button>
+      <p>Count is {count}</p>
+      <button onClick={handleDecrement}>-</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
